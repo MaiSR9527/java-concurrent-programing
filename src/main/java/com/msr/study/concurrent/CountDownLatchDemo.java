9da3e5a1604a09149a014fe1d92baa07d53f6004
srMai <maisrcn@qq.com>
2021-08-26 15:51:13 +0800

    一些修改

--- src/main/java/com/msr/study/concurrent/CountDownLatchDemo.java
+++ src/main/java/com/msr/study/concurrent/CountDownLatchDemo.java
@@ -11,20 +11,29 @@ import java.util.concurrent.TimeUnit;
 
 public class CountDownLatchDemo {
 
-    public static void main(String[] args) {
-        CountDownLatch countDownLatch = new CountDownLatch(10);
+    private static final int MAX_THREAD = 10;
 
-        for (int i = 0; i < 10; i++) {
-            new Thread(()->{
+    public static void main(String[] args) {
+        CountDownLatch countDownLatch = new CountDownLatch(MAX_THREAD);
+
+        for (int i = 0; i < MAX_THREAD; i++) {
+            new Thread(() -> {
                 try {
                     TimeUnit.SECONDS.sleep(2);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
                 System.out.println(Thread.currentThread().getName());
-            },"thread"+i).start();
-            countDownLatch.countDown();
+                countDownLatch.countDown();
+            }, "thread" + i).start();
+
         }
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("over");
     }
 
 }
